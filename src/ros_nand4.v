a1a25d20d9b9d288ae284216df14295755728e78
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2023-10-12 21:12:17 +0200

    Implemented ring oscillator with NAND2 with sub threshold

--- src/ros_nand4.v
+++ src/ros_nand4.v
@@ -1,12 +1,12 @@
 `default_nettype none
 
-module ros_nand4 #( parameter STAGES = 13) (
+module ros_nand4 #( parameter STAGES = 33) (
     input  wire       ena,      // will go high when the design is enabled
     output wire       clk       // clock
 );
-    wire [STAGES - 1:0] nets;
+    (* keep = "true" *) wire [STAGES - 1:0] nets;
 
-    assign clk = nets[0];
+    assign clk = !nets[0];
 
     (* keep = "true" *) sky130_fd_sc_hd__nand4_1 fstage (.A(nets[STAGES - 1]), .B(ena), .C(nets[STAGES - 1]), .D(nets[STAGES - 1]), .Y(nets[0]));
     genvar i;
--- src/tt_um_gfg_development_tos.v
+++ src/tt_um_gfg_development_tos.v
@@ -19,11 +19,14 @@ module tt_um_gfg_development_tos (
     assign uo_out[6:0]  = 8'b1111111;
     assign uio_out      = 8'b11111111;
 
+    assign uo_out[6]    = nan2_sub_clk;
     assign uo_out[7]    = nand4_clk;
 
     wire nand4_clk;
-
     ros_nand4 ros_nand4(.ena(ena), .clk(nand4_clk));
+
+    wire nand2_sub_clk;
+    ros_nand2_sub ros_nand2_sub(.ena(ena), .clk(nand2_sub_clk));
 
     always @(posedge clk) begin
         if (reset) begin
