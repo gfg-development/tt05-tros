d913753ab02f10505e8256ba4fd4fc906045ce1b
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2023-10-12 20:58:47 +0200

    Added missing variable decleration

--- src/ros_nand4.v
+++ src/ros_nand4.v
@@ -9,8 +9,9 @@ module ros_nand4 #( parameter STAGES = 13) (
     assign clk = nets[0];
 
     (* keep = "true" *) sky130_fd_sc_hd__nand4_1 fstage (.A(nets[STAGES - 1]), .B(ena), .C(nets[STAGES - 1]), .D(nets[STAGES - 1]), .Y(nets[0]));
+    genvar i;
     generate
-		for (i = 1; i < STAGES; i= i + 1) begin
+		for (i = 1; i < STAGES; i = i + 1) begin
 			(* keep = "true" *) sky130_fd_sc_hd__nand4_1 stage (.A(nets[i - 1]), .B(nets[i - 1]), .C(nets[i - 1]), .D(nets[i - 1]), .Y(nets[i]));
 		end
   	endgenerate
