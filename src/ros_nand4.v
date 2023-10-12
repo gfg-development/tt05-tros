621a8f309ce9aa3c95a6824f5463fa4c80fe1fb7
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2023-10-12 20:51:47 +0200

    Implemented a basic NAND4 based ring oscillator

---
+++ src/ros_nand4.v
@@ -0,0 +1,17 @@
+`default_nettype none
+
+module ros_nand4 #( parameter STAGES = 13) (
+    input  wire       ena,      // will go high when the design is enabled
+    output wire       clk       // clock
+);
+    wire [STAGES - 1:0] nets;
+
+    assign clk = nets[0];
+
+    (* keep = "true" *) sky130_fd_sc_hd__nand4_1 fstage (.A(nets[STAGES - 1]), .B(ena), .C(nets[STAGES - 1]), .D(nets[STAGES - 1]), .Y(nets[0]));
+    generate
+		for (i = 1; i < STAGES; i= i + 1) begin
+			(* keep = "true" *) sky130_fd_sc_hd__nand4_1 stage (.A(nets[i - 1]), .B(nets[i - 1]), .C(nets[i - 1]), .D(nets[i - 1]), .Y(nets[i]));
+		end
+  	endgenerate
+endmodule
